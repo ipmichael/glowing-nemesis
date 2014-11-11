7c10d66ba91dd0b40dcb419de3d876d4bcc1788b
Michael Ip <ip.w.michael@gmail.com>
2014-11-11 16:08:50 -0500

    tickbar with animate

--- game.js
+++ game.js
@@ -2,14 +2,24 @@ $(document).ready(function(){
 
 	var aCount = 0;
 	var inc = 1;
-	var tickVal = 37;
+	var tickVal = 0;
+	$('div#tickbar').hide();
 
 	$('div#boot').click(function(e) {
+		$('div#tickbar').hide();
     	var oldText = $(this).text();
     	aCount+=inc;
         // $(this).text('clicked');
         $('#foodLabel').text("Food Gathered: " + aCount);
+        loadbar();
+        if(tickVal == 10){
+        	tickVal = 0;
+        }
     });
+
+    function loadbar(){
+    	$('div#tickbar').animate({width:'toggle'},350);
+    }
 
     $('div#boot').hover(function() {
     	//set multiple CSS properties at once
