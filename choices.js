d649dacf71a7d3d2ef223545a8dca853a902a9b4
Michael Ip <ip.w.michael@gmail.com>
2014-12-24 01:25:19 -0500

    y isnt this working

--- choices.js
+++ choices.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
+	var storyAry = [];
+	var choice1 = 0;
+	var choice2 = 0;
+	var choice3 = 0;
 
-}
+	$('div#box1').click(function(e) {
+		choice1++;
+		update();
+    });
+
+	$('div#box2').click(function(e) {
+		choice2++;
+		update();
+    });
+
+    $('div#box3').click(function(e) {
+		choice3++;
+		update();
+    });
+
+    function update(){
+    	$('div#choice1').text(""+choice1);
+    	$('div#choice2').text(""+choice2);
+    	$('div#choice3').text(""+choice3);
+    }
+});
